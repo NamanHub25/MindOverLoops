bdd9dbe3da7f13a8d9729fbae58fd391ce0795b2
Naman Mehrotra <namanlinkedin183@gmail.com>
2026-01-20 19:25:06 +0530

    Koko Banana

---
+++ Array_Problems/Countevenno.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Countevenno {
+
+    public static int counteven(int[] nums, int n){
+        int cnt_no = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];   
+            int cnt = 0;
+
+            // if (num == 0) {
+            //     cnt = 1;      
+            // } else {
+                while (num > 0) {
+                    cnt++;
+                    num /= 10;
+                }
+                if (cnt % 2 == 0) {
+                cnt_no++;
+            }
+            }
+
+            return cnt_no;
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = input.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = input.nextInt();
+        }
+
+        int result = counteven(nums, n);
+        System.out.println("Number of elements with even number of digits in the array "
+                + Arrays.toString(nums) + " is: " + result);
+
+        input.close();
+    }
+}
---
+++ Binary_Search_programs/Koko_banana.java
@@ -0,0 +1,45 @@
+package Binary_Search_programs;
+
+import java.util.*;
+
+class Koko_banana {
+
+    public static int calculateTotalhours(int[] nums, int speed){
+        int totalhours = 0;
+
+        for(int pile: nums){
+            totalhours += (int)Math.ceil((double)pile/speed);
+        }
+        return totalhours;
+    }
+    public static int minEatingspeed(int[] nums, int n, int h){
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+
+        for(int i = 1; i <= maxVal; i++){
+            int hours = calculateTotalhours(nums, i);
+
+            if(hours < h){
+                return i;
+            }
+        }
+        return maxVal;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+
+        int n = input.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < n; i++){
+            nums[i] = input.nextInt();
+        }
+        System.out.print("Enter the minimum hours koko needs to finish bananas: ");
+        int h = input.nextInt();
+
+        int result = minEatingspeed(nums, n, h);
+        System.out.print("Minimum hours in which koko can finish all the bananas is: " + result);
+        input.close();
+    }
+}
